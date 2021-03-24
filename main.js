bac20b9a0e5a77c8b3f319832835e35b90f89b68
Robert Gumeny <robert.gumeny528@gmail.com>
2021-03-24 16:20:26 -0600

    centered on boise

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIwzALxUPNbatRBj3Xi1Uhp0fFzwWNBkE&libraries=places">
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.8688, lng: 151.2195 },
+    center: { lat: 43.615, lng: 116.2023 },
     zoom: 13,
   });
   const input = document.getElementById("pac-input");
